d0463e1bb4b9974d4e911b7e86414bf5084d0cce
Amanda D'Argenio <dargenioa14@gmail.com>
2021-01-09 17:06:17 -0500

    added post and put request in api.js

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,23 @@ const router = require("express").Router();
 const Workout = require("../models/Workout");
 
 router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
-  module.exports =  router;
+router.put("/api/workouts/:id", ({ body }, res) => {
+  Workout.update(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
