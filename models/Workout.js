9a9f872a9463ca2556d629993e082671ed0000f8
Amanda D'Argenio <dargenioa14@gmail.com>
2021-01-12 18:47:41 -0500

    removed exercise model and added to array in workout.js

--- models/Workout.js
+++ models/Workout.js
@@ -5,20 +5,50 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
   day: {
     type: Date,
-    default: Date.now
+    default: () => new Date()
   },
   exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise",
-    },
-  ],
-  totalDuration: {
-    type: Number,
-    default: 0
-
+      type: {
+        type: String,
+        trim: true,
+        required: "Please enter the type of workout, (resistance, cardio, etc..)",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please enter the name of workout",
+      },
+      duration: {
+        type: Number,
+        required: "Please enter the duration",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      }
+    }
+  ]
+},
+  {
+    toJSON: {
+      virtuals: true
+    }
   }
-  
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0)
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
-const db = require("../models");
+const Workout = require("../models/Workout");
+
+//continue workout and new workout, makes a new workout and a new exercise
+//it is not targeting a workout and adding another exeri
+
+// View the total duration of each workout from the past seven workouts on the stats page.
+
+// Important: Look into using a MongoDB aggregate function to dynamically add up and return the total duration for each workout.
+// Check out the MongoDB documentation on the $addFields,
+// the MongoDB documentation on the $sum operator,
+// and the Mongoose documentation on aggregate functions to learn how it can be accomplished.
 
 router.post("/api/workouts", ({ body }, res) => {
-  db.Workout.create(body)
+  Workout.create(body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
@@ -11,20 +21,22 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.put("/api/workouts/:id", ({ body }, res) => {
-  db.Exercise.create(body)
-    .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true })
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
     .catch(err => {
       res.json(err);
     });
-});
+  });
 
 
 router.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
+  Workout.find({})
   .populate("exercises")
   .then(dbWorkout => {
     res.json(dbWorkout);
@@ -34,9 +46,24 @@ router.get("/api/workouts", (req, res) => {
   });
 });
 
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([{
+    $addFields: {
+      totalDuation: {
+        $sum: "$exercises.duration"
+      }
+    }
+  }])
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({})
-  .populate("exercises")
+  Workout.find({})
   .then(dbWorkout => {
     res.json(dbWorkout);
   })
